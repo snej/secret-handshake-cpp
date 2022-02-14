9da99ce0cacb705455b34425e14641dae749fc5c
Jens Alfke <jens@mooseyard.com>
2022-02-14 11:26:15 -0800

    CMake Mac x86 compatiblity. Custom test reporter.

--- build_and_test.sh
+++ build_and_test.sh
@@ -3,5 +3,13 @@
 mkdir -p build_cmake
 cd build_cmake
 cmake ..
-make -j 8 SecretHandshakeTests
-./SecretHandshakeTests
+cmake --build .
+
+echo "Running tests..."
+
+if [ -e Debug/SecretHandshakeTests.exe ]
+then
+    Debug/SecretHandshakeTests.exe -r list
+else
+    ./SecretHandshakeTests -r list
+fi
